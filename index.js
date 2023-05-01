0fe88bfe19ba8febeb21bddf11578bf72b510cce
AnikaBushra <anikabushra024@gmail.com>
2023-05-01 08:34:52 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = process.env.PROT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('hello')
+})
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
